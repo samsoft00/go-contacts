15ff13ce616c0e6c8035b4e6bc5a9c347d3950ac
Oyewole Samuel A <oyewoleabayomi@gmail.com>
2021-10-07 23:53:07 +0100

    fix lint issue

--- main.go
+++ main.go
@@ -10,6 +10,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+const (
+	RandNum = 10
+	AbortStatus = 204
+)
+
 type contact struct {
 	ID				string		`json:"id"`
 	FullName		string		`json:"fullname"`
@@ -22,7 +27,7 @@ type contact struct {
 var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 var contacts = []contact {}
 
-// cor middleware
+// cor middleware.
 func CORSMiddleware() gin.HandlerFunc {
     return func(c *gin.Context) {
         c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
@@ -31,9 +36,10 @@ func CORSMiddleware() gin.HandlerFunc {
         c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
 
         if c.Request.Method == "OPTIONS" {
-            c.AbortWithStatus(204)
+            c.AbortWithStatus(AbortStatus)
 			return
         }
+
         c.Next()
     }
 }
@@ -41,20 +47,22 @@ func CORSMiddleware() gin.HandlerFunc {
 func generateRandID(n int) string {
     b := make([]rune, n)
     for i := range b {
+		
         b[i] = letters[rand.Intn(len(letters))]
     }
+
     return string(b)	
 }
 
-//getContacts responds with the list of all albums as JSON
+// getContacts responds with the list of all albums as JSON.
 func getContacts(c *gin.Context) {
 	c.IndentedJSON(http.StatusOK, contacts)
 }
 
-// postContacts adds an album from JSON received in the request body
+// postContacts adds an album from JSON received in the request body.
 func postContacts(x *gin.Context) {
 	var newContact contact
-	newContact.ID = generateRandID(10)
+	newContact.ID = generateRandID(RandNum)
 	newContact.Createddate = time.Now().String()
 
 	if err := x.BindJSON(&newContact); err != nil {
@@ -68,13 +76,14 @@ func postContacts(x *gin.Context) {
 func getContactByID(c *gin.Context) {
 	id := c.Param("id")
 
-	//loop over the list of albums, looking for an album whose ID value matches the parameter
+	// loop over the list of albums, looking for an album whose ID value matches the parameter.
 	for _, a := range contacts {
 		if a.ID == id {
 			c.IndentedJSON(http.StatusOK, a)
 			return
 		}
 	}
+
 	c.IndentedJSON(http.StatusNotFound, gin.H{"message": "album not found"})
 }
 
@@ -101,6 +110,6 @@ func main(){
 	port := os.Getenv("PORT")
 	if port == "" { log.Fatal("$PORT must be set") }
 
-	//nolint:errcheck //CODEI8:
+	//nolint:errcheck //CODEI8: no better solution 
 	router.Run(":" + port)
 }
